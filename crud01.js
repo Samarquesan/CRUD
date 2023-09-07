c71239d78b408cfdec926a04d0f1096c7a2952e5
Samuel Marques dos Santos <78937543+Samarquesan@users.noreply.github.com>
2023-09-07 14:47:38 -0300

    Update crud01.js

--- crud01.js
+++ crud01.js
@@ -31,25 +31,25 @@ CRUD
     }
     createPost({owner: 'Samsimic', content: 'Segundo tweet'});
     createPost({owner: 'Samsimic', content: 'Terceiro tweet'});
-    console.log(miniTwitter.posts) [{owner: 'Samsimic', content: 'Meu primeiro tweet'}]
+    //console.log(miniTwitter.posts) [{owner: 'Samsimic', content: 'Meu primeiro tweet'}]
 
     //READ
     function getPosts(){
         return miniTwitter.posts;
     }
-    console.log(getPosts()) [{id: 1, owner: 'Samsimic', content: 'Meu primeiro tweet'}]
+    console.log(getPosts()) //[{id: 1, owner: 'Samsimic', content: 'Meu primeiro tweet'}]
 
     //UPDATE
     function updateContentPost(id, novoConteudo){
         const updatedPost = getPosts().find((post) => {
             return post.id === id;
-        })
-        console.log(updatedPost) [{ id: 1, owner: 'Samsimic', content: 'Meu primeiro tweet'}]
+        });
+        console.log(updatedPost) //[{ id: 1, owner: 'Samsimic', content: 'Meu primeiro tweet'}]
         updatedPost.content = novoConteudo
     } 
 
     updateContentPost(1,'Novo conteúdo do tweet')
-    console.log(getPosts()) [{id: 1, owner: 'Samsimic', content:'Novo tweet'}]
+    console.log(getPosts()) //[{id: 1, owner: 'Samsimic', content:'Novo tweet'}]
 
     //DELETE
     function deletePost(id){
@@ -62,4 +62,4 @@ CRUD
     deletePost(1);
     deletePost(2);
     deletePost(3);
-    console.log(getPosts()); [{id: 3, owner: 'Samsimic', content:'Terceiro Tweet'}]
+    console.log(getPosts()); //[{id: 3, owner: 'Samsimic', content:'Terceiro Tweet'}]
